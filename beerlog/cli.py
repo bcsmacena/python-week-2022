b75363b40295843ba0400c1a4285f099165b4a21
bcsmacena <bcsmacena@yahoo.com.br>
2022-05-04 20:07:11 +0000

    aula 2 0002

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,32 @@
-from .config import settings
+# import sys
+# from .config import settings
 
 
-def main():
-    print("Hello from", settings.NAME)
+# def main():
+#     print("Hello from", settings.NAME)
+#     print(sys.argv[1:])
+
+import typer
+from beerlog.core import add_beer_to_database
+
+main = typer.Typer(help="Beer Application")
+
+@main.command("add")
+def add(
+        name: str,
+        style: str,
+        flavor: int = typer.Option(...),
+        image: int = typer.Option(...),
+        cost: int = typer.Option(...)
+    ):
+    """Adds a new beer to database"""
+    if add_beer_to_database(name, style, flavor, image, cost):
+        print("beer added to database")
+    else: 
+        print("no entry")
+
+
+@main.command("list")
+def list_beers(style: str):
+    """List beers to database"""
+    print(style)
