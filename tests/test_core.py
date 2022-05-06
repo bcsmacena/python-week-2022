58ac01b66446e6eb4a3b13a823f444a946cb22d3
bcsmacena <bcsmacena@yahoo.com.br>
2022-05-06 21:15:57 +0000

    aula 3 testes unitarios

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,5 +4,9 @@ def test_add_beer_to_database():
     assert add_beer_to_database("Blue Moon", "Witbier", 10, 3, 6)
 
 def test_get_beers_from_database():
+    # Arrange
+    assert add_beer_to_database("Blue Moon", "Witbier", 10, 3, 6)
+    # Act
     results = get_beers_from_database()
+    # Assert    
     assert len(results) > 0
